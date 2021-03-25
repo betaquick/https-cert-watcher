89d698ed98cdb8ad7530cf754fa24caa1e214dbf
Idiakose O. Sunday <idiakosesunday@gmail.com>
2021-03-25 19:08:57 +0100

    Add sample code

---
+++ example/sample.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const { createServer } = require('../src');
+
+const badOpts = {
+  key: fs.readFileSync(path.join(__dirname, '../test//keys/good_privkey.pem')),
+  cert: fs.readFileSync(path.join(__dirname, '../test/keys/good_cert.pem')),
+  ca: fs.readFileSync(path.join(__dirname, '../test/keys/good_chain.pem'))
+};
+
+const badKeyPaths = [
+  path.join(__dirname, '../test/keys/good_cert.pem'),
+  path.join(__dirname, '../test//keys/good_chain.pem'),
+  path.join(__dirname, '../test/keys/good_privkey.pem')
+];
+
+const server = createServer(badOpts, badKeyPaths, (req, res) => {
+  // eslint-disable-next-line no-console
+  console.log('connect');
+  res.writeHead(200);
+  res.write('Hello World!\n');
+  res.end('Goodbye World!\n');
+}, 20);
+
+
+server.listen(9999, () => console.log('Server up: ', server.address()));
