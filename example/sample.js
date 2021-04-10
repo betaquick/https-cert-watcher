25b4e297b9a9499beb93cd38cd1bf38d33325a50
Idiakose O. Sunday <idiakosesunday@gmail.com>
2021-04-10 06:00:31 +0100

    Update sample

--- example/sample.js
+++ example/sample.js
@@ -3,19 +3,21 @@ const path = require('path');
 
 const { createServer } = require('../src');
 
-const badOpts = {
-  key: fs.readFileSync(path.join(__dirname, '../test//keys/good_privkey.pem')),
-  cert: fs.readFileSync(path.join(__dirname, '../test/keys/good_cert.pem')),
-  ca: fs.readFileSync(path.join(__dirname, '../test/keys/good_chain.pem'))
-};
+function serverOptsFactory() {
+  return {
+    key: fs.readFileSync(path.join(__dirname, '../test/keys/good_privkey.pem')),
+    cert: fs.readFileSync(path.join(__dirname, '../test/keys/good_cert.pem')),
+    ca: fs.readFileSync(path.join(__dirname, '../test/keys/good_chain.pem'))
+  };
+}
 
-const badKeyPaths = [
+const KeyPaths = [
   path.join(__dirname, '../test/keys/good_cert.pem'),
   path.join(__dirname, '../test//keys/good_chain.pem'),
   path.join(__dirname, '../test/keys/good_privkey.pem')
 ];
 
-const server = createServer(badOpts, badKeyPaths, (req, res) => {
+const server = createServer(serverOptsFactory, KeyPaths, (req, res) => {
   // eslint-disable-next-line no-console
   console.log('connect');
   res.writeHead(200);
